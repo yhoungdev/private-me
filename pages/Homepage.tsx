ea1f9c94a48721888d5dc20c94b1510594396daa
Sudo Whoam i <yhoungdev@gmail.com>
2022-10-18 06:12:22 +0100

    Feat: added Circle CI config

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -2,15 +2,14 @@ import DefaultHeader from "../Components/main/DefaultHeader";
 import Hero from "../Components/major/Hero";
 import Info from "../Components/major/Info";
 
-const  Homepage =() => {
-    return (
-        <>
-            <DefaultHeader/>
-            <Hero/>
-            <Info/>
-
-        </>
-    )
-}
+const Homepage = () => {
+  return (
+    <>
+      <DefaultHeader />
+      <Hero />
+      <Info />
+    </>
+  );
+};
 
 export default Homepage;
