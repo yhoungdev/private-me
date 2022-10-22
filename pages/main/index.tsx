78a13cb81756b324542ade66e38f729be16f7089
Keerthana Kasthuril <keerthana.kasthuril@thoughtworks.com>
2022-10-22 13:01:19 +0530

    General UI fixes

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,45 +1,30 @@
 import { Box, Flex } from "@chakra-ui/react";
-import CreatePost from "../../Components/essentials/CreatePost";
-import Header from '../../Components/major/Header';
-import ContainerLayout from '../../Layouts/ContainerLayout';
-import PostContainer from '../../Components/essentials/PostContainer';
-import CommunityBox from '../../Components/major/CommunitiesBox';
+import Header from "../../Components/major/Header";
+import ContainerLayout from "../../Layouts/ContainerLayout";
+import PostContainer from "../../Components/essentials/PostContainer";
+import CommunityBox from "../../Components/major/CommunitiesBox";
 import CreatePostPanel from "../../Components/main/CreatePostBox";
-import OthersComment from '../../Components/major/OthersComment';
 
-const Index =() => {
+const Index = () => {
+  return (
+    <>
+      <Header />
 
-    return (
-        <>
-            <Header/>
-            
-            <ContainerLayout>
+      <ContainerLayout>
+        <Flex flexDirection={["column-reverse", "row"]} gap={"1em"}>
+          <Box w={["100%", "70%"]}>
+            <CreatePostPanel />
+            <PostContainer item={"block"} />
+            <PostContainer item={"none"} />
+          </Box>
 
-                
+          <Box display={["none", "block"]}>
+            <CommunityBox />
+          </Box>
+        </Flex>
+      </ContainerLayout>
+    </>
+  );
+};
 
-                <Flex flexDirection={['column-reverse' , 'row']}
-                    gap={'1em'}>
-
-                   <Box w={['100%','70%']}>
-                     <CreatePostPanel/>
-                     <PostContainer item={'block'}/>
-                     <PostContainer item={'none'}/>
-                   </Box>
-
-                    <Box display={['none' , 'block']}>
-                        <CommunityBox/>
-                    </Box>
-
-                </Flex>
-
-
-
-
-            </ContainerLayout>
-            
-        </>
-    )
-}
-
-
-export default Index; 
+export default Index;
