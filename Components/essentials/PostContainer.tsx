b63096e16e55b33fcdf7db7de93ca375e2de2da0
Sudo Whoam i <54102389+yhoungdev@users.noreply.github.com>
2023-02-11 04:20:11 +0100

    updated items

--- Components/essentials/PostContainer.tsx
+++ Components/essentials/PostContainer.tsx
@@ -1,62 +1,50 @@
-import {Box, Text, Avatar  } from '@chakra-ui/react';
-import PostComment from '../major/PostComment';
-import {AiOutlineComment} from 'react-icons/ai';
-import { useState } from 'react';
-
+import { Box, Text, Avatar, Flex } from "@chakra-ui/react";
+import PostComment from "../major/PostComment";
+import { AiOutlineComment } from "react-icons/ai";
+import { useState } from "react";
 
 interface Props {
-    item ?: string
+  item?: string;
 }
-const PostContainer =({item}: Props) => {
-    //@ts-ignore
-    const [ data , setData ] = useState();
-    
+const PostContainer = ({ item }: Props) => {
+  //@ts-ignore
+  const [data, setData] = useState();
 
-    return (
-        <>
-        
-            <Box p={'1.6em'} bg={'var(--dark-shade)'}
-                my={'1em'} borderRadius={'0.5em'}    fontFamily={'var(--font-commic)'}>
-                
-                <Box display={'flex'} alignItems={'center'} gap={'1em'}>
-
-                    <Avatar />
-                    <Box>
-                        <Text>Username</Text>
-                        <Text fontSize={'0.8em'}>14th OCtober, 2022</Text>
-                    </Box>
-
-                </Box>
-
-                <Box my={'1em'}>
-
-                    <Text my={'0.5em'}>
-                        Edward Joseph Snowden (born June 21, 1983) is an American former computer intelligence 
-                    </Text>
-
-
-                </Box>
-
-
-                <Box my={'1em'} borderBottom={'1px solid gray'} pb={'1em'}>
-                    <Box display={'flex'} gap={'0.5em'}
-                     alignItems={'center'}>
-                        <AiOutlineComment size={'1.2em'}/>
-                        <Text>100</Text>
-                    </Box>
-                </Box>
-                {/* post container component */}
-                <Box display={item}>
-                    
-                        <PostComment/> : ''
-                    
-                </Box>
-
+  return (
+    <>
+      <Box
+        p={"1.6em"}
+        bg={"var(--dark-shade)"}
+        my={"1em"}
+        borderRadius={"1em"}
+        fontFamily={"var(--font-commic)"}
+      >
+        <Flex justifyContent={"space-between"} alignItems={"center"}>
+          <Box display={"flex"} alignItems={"center"} gap={"1em"}>
+            <Avatar />
+            <Box>
+              <Text>Username</Text>
+              <Text fontSize={"0.8em"}> </Text>
             </Box>
-        
-        </>
-    )
-}
+          </Box>
 
+          <Box>02/05/2023</Box>
+        </Flex>
 
-export default  PostContainer;
+        <Box my={"1em"} px={["1em", "2em"]}>
+          <Text my={"0.5em"} fontSize={"16px"}>
+            Edward Joseph Snowden (born June 21, 1983) is an American former
+            computer intelligence
+          </Text>
+
+          <Flex gap={"0.5em"} my={"1em"}>
+            <AiOutlineComment size={"1.3em"} />
+            <Text>399k</Text>
+          </Flex>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default PostContainer;
--- Components/major/CommunitiesBox.tsx
+++ Components/major/CommunitiesBox.tsx
@@ -1,14 +1,17 @@
-import { Box , Text} from "@chakra-ui/react";
-import  CommunityData from './CommunityData'
-const CommunityBox =() => {
-    return (
-        <>
-            <Box p={'2em'} bg={'var(--dark-shade)'}>
-                <Text my={'0.5em'} fontWeight={'bold'}> Top Communities  </Text>
-                <CommunityData/>
-            </Box>
-        </>
-    )
-}
+import { Box, Text } from "@chakra-ui/react";
+import CommunityData from "./CommunityData";
+const CommunityBox = () => {
+  return (
+    <>
+      <Box p={"2em"} bg={"var(--dark-shade)"} w={"100%"}>
+        <Text my={"0.5em"} fontWeight={"bold"}>
+          {" "}
+          Top Communities{" "}
+        </Text>
+        <CommunityData />
+      </Box>
+    </>
+  );
+};
 
 export default CommunityBox;
